59664e9196dd1b5c1df03ff453d6d3503c72f921
lightweigh <827907347@qq.com>
2023-11-13 16:05:40 +0800

    finished assignment1/Q3_softmax (unresolved)

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -34,7 +34,18 @@ def softmax_loss_naive(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    train_num = X.shape[0]  # num samples
+
+    for i in range(train_num):
+        y_hat = X[i] @ W  # raw scores vector
+        y_exp = np.exp(y_hat - y_hat.max())  # numerically stable exponent vector
+        softmax = y_exp / y_exp.sum()  # pure softmax for each score
+        loss -= np.log(softmax[y[i]])  # append cross-entropy
+        softmax[y[i]] -= 1  # update for gradient
+        dW += np.outer(X[i], softmax)  # gradient
+
+    loss = loss / train_num + reg * np.sum(W ** 2)  # average loss and regularize
+    dW = dW / train_num + 2 * reg * W  # finish calculating gradient
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -59,7 +70,17 @@ def softmax_loss_vectorized(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    train_num = X.shape[0]  # number of samples
+    Y_hat = X @ W  # raw scores matrix
+
+    P = np.exp(Y_hat - Y_hat.max())  # numerically stable exponents
+    P /= P.sum(axis=1, keepdims=True)  # row-wise probabilities (softmax)
+
+    loss = -np.log(P[range(train_num), y]).sum()  # sum cross entropies as loss
+    loss = loss / train_num + reg * np.sum(W ** 2)  # average loss and regularize
+
+    P[range(train_num), y] -= 1  # update P for gradient
+    dW = X.T @ P / train_num + 2 * reg * W  # calculate gradient
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
